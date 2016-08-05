e91ff12137ba30c490820a582ac6821226ec2407
Jake Priddy <priddyjacob84@gmail.com>
2016-08-05 17:07:29 -0400

    8/5 1707

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-$(document).ready(function() {
-  $(".shopping-item-delete").click(function() {
-               console.log('delete works');
-               });  
-  $(".shopping-item-toggle").click(function() {
-               console.log('check works');
-               });  
-  $("#js-shopping-list-form").submit(function(event) {
-      console.log('submit works');
-       event.preventDefault();
-  });
+$(document).ready(function () {
+    $(".shopping-item-delete").click(function () {
+        console.log('delete works');
+    });
+    $(".shopping-item-toggle").click(function () {
+        console.log('check works');
+        //  if ($())             
+    });
+    $("#js-shopping-list-form").submit(function () {
+        $('.shopping-list').append('<li> <span class = "shopping-item" > '+'
+            $('shopping-list-entry').val()
+            '+' < /span> < div class = "shopping-item-controls" > < button class = "shopping-item-toggle" > < span class = "button-label" > check < /span> < /button> < button class = "shopping-item-delete" > < span class = "button-label" > delete < /span> < /button> < /div> < /li>');
+            console.log('submit works'); event.preventDefault(););
+    });
 });
 //delete =   shopping-item-delete
 //check =    shopping-item-toggle
-//add item = button type="submit
+//add item = button type="submit -- add item to the list (think .append())
+//$('.shopping-list').append('<li>
+//        <span class="shopping-item">'+'$('shopping-list-entry').val()'+'</span>
+//        <div class="shopping-item-controls">
+//          <button class="shopping-item-toggle">
+//            <span class="button-label">check</span>
+//          </button>
+//          <button class="shopping-item-delete">
+//            <span class="button-label">delete</span>
+//          </button>
+//        </div>
+//      </li>');
