6bc3c8b949b859cfc2e33b880088941d25644a0a
Jake Priddy <priddyjacob84@gmail.com>
2016-08-11 11:57:34 -0400

    8/11 1157

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 $(document).ready(function () {
-    $(".shopping-item-delete").click(function () {
-        console.log('delete works');
-		
-    });
+	//whenever you are changing something that isn't there yet ( but will be....)
+	$('.shopping-list').on('click', '.shopping-item-delete', function () {
+		console.log("new delete works");
+		//actually delete
+	});
 	//strike through
 	//http://www.w3schools.com/jsref/jsref_strike.asp
-    $(".shopping-item-toggle").click(function () {
-        console.log('check works');
-    });
-    $("#js-shopping-list-form").submit(function () {
-        console.log('submit works');
+	$('.shopping-list').on('click', '.shopping-item-toggle', function() {
+		console.log('new check works');
+		//actually line through
+
+	});
+	$("#js-shopping-list-form").submit(function () {
+		console.log('submit works');
 		event.preventDefault();
 		var groceryThing = $('#shopping-list-entry').val();
 		console.log(groceryThing);
-		//add new li element
 		$('.shopping-list').append('<li><span class="shopping-item">'+ groceryThing +'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>') });
+
+
+
 });
+
 //delete =   shopping-item-delete
 //check =    shopping-item-toggle
 //add item = button type="submit -- add item to the list (think .append())
