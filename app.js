f8d8d45f073691f021ff776576c58c6f8a30aeab
Jake Priddy <priddyjacob84@gmail.com>
2016-08-11 11:38:13 -0400

    8/11 1137

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ $(document).ready(function () {
         console.log('delete works');
 		
     });
+	//strike through
+	//http://www.w3schools.com/jsref/jsref_strike.asp
     $(".shopping-item-toggle").click(function () {
         console.log('check works');
-        //  if ($())             
     });
     $("#js-shopping-list-form").submit(function () {
-        console.log('submit works');		event.preventDefault();
-    });
+        console.log('submit works');
+		event.preventDefault();
+		var groceryThing = $('#shopping-list-entry').val();
+		console.log(groceryThing);
+		//add new li element
+		$('.shopping-list').append('<li><span class="shopping-item">'+ groceryThing +'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>') });
 });
 //delete =   shopping-item-delete
 //check =    shopping-item-toggle
