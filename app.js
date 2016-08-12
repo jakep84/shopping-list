6fc64df2893e69fd8fbecbf32ca7cbba46fa5a38
Jake Priddy <priddyjacob84@gmail.com>
2016-08-12 17:44:05 -0400

    8/12 0543

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
 	//whenever you are changing something that isn't there yet ( but will be....)
 	$('.shopping-list').on('click', '.shopping-item-delete', function () {
-		console.log("new delete works");
-		//actually delete
+	
+		$(this).closest("li").remove();
+			console.log("new delete works");
+		//actually remove li http://www.w3schools.com/jquery/jquery_dom_remove.asp
+		//find "closest"
 	});
 	//strike through
 	//http://www.w3schools.com/jsref/jsref_strike.asp
 	$('.shopping-list').on('click', '.shopping-item-toggle', function() {
-		console.log('new check works');
-		//actually line through
+		$(this).closest('li').children('.shopping-item').css('text-decoration','line-through');
+				console.log('new line-through works');
+		//"toggle, switch, swap" line through
 
 	});
 	$("#js-shopping-list-form").submit(function () {
@@ -16,10 +20,8 @@ $(document).ready(function () {
 		event.preventDefault();
 		var groceryThing = $('#shopping-list-entry').val();
 		console.log(groceryThing);
+		//append an entire new li with all the fixin's
 		$('.shopping-list').append('<li><span class="shopping-item">'+ groceryThing +'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>') });
-
-
-
 });
 
 //delete =   shopping-item-delete
